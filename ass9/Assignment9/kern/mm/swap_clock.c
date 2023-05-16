58401b4bdff4958672c152b170cf5b8399b654ce
ntdgy <ntdgy2001@gmail.com>
2023-05-16 17:50:56 +0800

    add ass9

---
+++ ass9/Assignment9/kern/mm/swap_clock.c
@@ -0,0 +1,110 @@
+#include <defs.h>
+#include <riscv.h>
+#include <stdio.h>
+#include <string.h>
+#include <swap.h>
+#include <swap_clock.h>
+#include <list.h>
+#include <pmm.h>
+
+
+list_entry_t pra_list_head, *curr_ptr;
+
+static int
+_clock_init_mm(struct mm_struct *mm)
+{     
+    //TODO
+    return 0;
+}
+
+static int
+_clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
+{
+    //TODO
+    return 0;
+}
+
+
+static int
+_clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
+{
+    //TODO
+    return 0;
+}
+
+
+static int
+_clock_check_swap(void) {
+
+    cprintf("---------Clock check begin----------\n");
+    cprintf("write Virt Page c in clock_check_swap\n");
+    *(unsigned char *)0x3000 = 0x0c;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page a in clock_check_swap\n");
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page d in clock_check_swap\n");
+    *(unsigned char *)0x4000 = 0x0d;
+    assert(pgfault_num==4);
+    cprintf("write Virt Page b in clock_check_swap\n");
+    *(unsigned char *)0x2000 = 0x0b;  
+    assert(pgfault_num==4);
+    cprintf("write Virt Page e in clock_check_swap\n");
+    *(unsigned char *)0x5000 = 0x0e;
+    assert(pgfault_num==5);
+    cprintf("write Virt Page b in clock_check_swap\n");
+    *(unsigned char *)0x2000 = 0x0b;
+    assert(pgfault_num==5);
+    cprintf("write Virt Page a in clock_check_swap\n");
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==6);
+    cprintf("write Virt Page b in clock_check_swap\n");
+    *(unsigned char *)0x2000 = 0x0b;
+    assert(pgfault_num==6);
+    cprintf("write Virt Page c in clock_check_swap\n");
+    *(unsigned char *)0x3000 = 0x0c;
+    assert(pgfault_num==7);
+    cprintf("write Virt Page d in clock_check_swap\n");
+    *(unsigned char *)0x4000 = 0x0d;
+    assert(pgfault_num==8);
+    cprintf("write Virt Page e in clock_check_swap\n");
+    *(unsigned char *)0x5000 = 0x0e;
+    assert(pgfault_num==9);
+    cprintf("write Virt Page a in clock_check_swap\n");
+    assert(*(unsigned char *)0x1000 == 0x0a);
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==9);
+    cprintf("Clock check succeed!\n");
+
+    return 0;
+}
+
+
+static int
+_clock_init(void)
+{
+    return 0;
+}
+
+static int
+_clock_set_unswappable(struct mm_struct *mm, uintptr_t addr)
+{
+    return 0;
+}
+
+static int
+_clock_tick_event(struct mm_struct *mm)
+{ return 0; }
+
+
+struct swap_manager swap_manager_clock =
+{
+     .name            = "clock swap manager",
+     .init            = &_clock_init,
+     .init_mm         = &_clock_init_mm,
+     .tick_event      = &_clock_tick_event,
+     .map_swappable   = &_clock_map_swappable,
+     .set_unswappable = &_clock_set_unswappable,
+     .swap_out_victim = &_clock_swap_out_victim,
+     .check_swap      = &_clock_check_swap,
+};
